6f03d53d56c9cc214cefb47c7bdbc15543072e95
Avram Eisner <avrame@gmail.com>
2019-09-22 13:33:42 -0700

    Restructured into a server and client app.

---
+++ client/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+fetch('api/ping', {
+  accept: "application/json"
+})
+  .then(checkStatus)
+  .then(parseJSON)
+  .then((json) => {
+    console.log(json)
+  });
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  const error = new Error(`HTTP Error ${response.statusText}`);
+  error.status = response.statusText;
+  error.response = response;
+  console.log(error); // eslint-disable-line no-console
+  throw error;
+}
+
+function parseJSON(response) {
+  return response.json();
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+// Express only serves static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/api/ping', function (req, res) {
+ return res.json({
+   foo: 'bar'
+ });
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
