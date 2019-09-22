7b1ba22b2152b85c58180e0ba8b8ad62ac1a237b
Avram Eisner <avrame@gmail.com>
2019-09-22 12:19:53 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PROT }`));
