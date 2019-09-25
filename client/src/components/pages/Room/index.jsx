2b6dc4d4eb04137bc9f1c7a8f1709c3abe573328
Avram Eisner <avrame@gmail.com>
2019-09-25 10:59:24 -0700

    trying specific path

--- client/src/components/pages/Room/index.jsx
+++ client/src/components/pages/Room/index.jsx
@@ -28,7 +28,7 @@ function Room({ match }) {
 
   useEffect(() => {
     const websocketURL = (window.location.host === 'localhost:3000')
-    ? 'ws://localhost:5000' : 'wss://chatter-box.herokuapp.com:5000';
+    ? 'ws://localhost:5000/websocket' : 'wss://chatter-box.herokuapp.com:5000/websocket';
     messageSocket = new WebSocket(websocketURL);
     return () => {
       messageSocket.close();
